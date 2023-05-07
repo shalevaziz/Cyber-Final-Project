32a724f8fc34d39c835592a7025a872e1c0d5659
shalev aziz <azizshalev@gmail.com>
2023-05-08 00:24:43 +0300

    proggress with GUI. finalizing share screen

--- FirstSetupGUI.py
+++ FirstSetupGUI.py
@@ -1,68 +1,151 @@
 import tkinter as tk
+from tkinter import font, messagebox
 from PIL import Image, ImageTk
 import json
 
-class SetupWindow(tk.Frame):
+class Main_Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1200x800")
+        self.title("First Setup")
+        self.resizable(False, False)
+        self.frames = {}
+        self.load_frames()
+        self.current_frame = 'main'
+        self.show_frame(self.current_frame)
+        self.mainloop()
+    
+    def load_frames(self):
+        self.frames['main'] = Main_Frame(self)
+        self.frames['edit'] = Edit_Frame(self)
+    
+    def show_frame(self, frame):
+        self.frames[self.current_frame].pack_forget()
+        self.frames[frame].pack(anchor='nw', fill='both', expand=True)
+        self.frames[frame].load_pcs()
+        self.current_frame = frame
+
+
+class Window(tk.Frame):
     def __init__(self, master=None):
         global PC_ICON_ONLINE, PC_ICON_OFFLINE, X_ICON
         self.master = master
-        self.master.geometry("1200x1200")
+        self.master.geometry("1200x800")
         self.master.title("First Setup")
-        self.pcs = {}
-        super().__init__(master, width = 1200, height = 1200)
+        self.master.resizable(False, False)
+        self.pcs_pos = {}
+        self.pcs = []
+        super().__init__(master, width = 1200, height = 800)
         
-        self.place(x = 0, y = 0)
         PC_ICON_ONLINE = ImageTk.PhotoImage(Image.open(r"basic server-client encrypted/icons/pc_icon_online.png").resize((100, 100)))
         PC_ICON_OFFLINE = ImageTk.PhotoImage(Image.open(r"basic server-client encrypted/icons/pc_icon_offline.png").resize((100, 100)))
         X_ICON = ImageTk.PhotoImage(Image.open(r"basic server-client encrypted/icons/x_icon.png").resize((20, 20)))
         self.load_pcs()
-
+    
     def load_pcs(self):
+        for pc in self.pcs:
+            if pc is not None:
+                pc.destroy()
+
+        self.pcs = []
+
         with open('locations.json', 'r') as f:
-            self.pcs = json.load(f)
-            
-        for pc in self.pcs.items():
-            self.create_PCIcon(pc[0], pc[1])
-            
+            self.pcs_pos = json.load(f)
+        
+        for pc in self.pcs_pos.items():
+            self.pcs.append(self.create_PCIcon(pc[0], pc[1]))
+    
     def create_PCIcon(self, mac, pos = (0,0)):
-        PCIcon(self, mac, pos, draggable = True)
-        self.pcs = {mac:(0,0)}
+        raise NotImplementedError('This function is not implemented in the base class')
+
+class Main_Frame(Window):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.create_widgets()
+        self.bind('<Button-1>', lambda event: self.dropdown.place_forget())
+
+    def create_widgets(self):
+        self.create_dropdown()
+        self.create_edit_button()
+
+    def create_dropdown(self):
+        self.dropdown = DropDownMenu(self)
+
+    def create_edit_button(self):
+        self.edit_button = tk.Button(self, text = 'Edit', command = lambda: self.master.show_frame('edit'))
+        self.edit_button.place(x = 1100, y = 700)
+
+    def assign_dropdown(self, mac):
+        pos = self.pcs_pos[mac]
+        self.dropdown.place(x = pos[0]+10, y = pos[1]+130)
+        self.dropdown.set_mac(mac)
+    
+    def create_PCIcon(self, mac, pos = (0,0)):
+        PCIcon_View_Mode(self, mac, pos)
+        self.pcs_pos[mac] = pos
+
+class Edit_Frame(Window):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.create_done_button()
+    
+    def create_done_button(self):
+        self.done_button = tk.Button(self, text = 'Save', command = lambda: self.master.show_frame('main'))
+        self.done_button.place(x = 1100, y = 700)
+
+    def create_PCIcon(self, mac, pos = (0,0)):
+        PCIcon_Edit_Mode(self, mac, pos)
+        self.pcs_pos = {mac:pos}
     
     def change_location(self, mac, pos):
-        self.pcs[mac] = tuple(pos)
+        self.pcs_pos[mac] = tuple(pos)
         with open('locations.json', 'w') as f:
-            json.dump(self.pcs, f)
+            json.dump(self.pcs_pos, f)
 
     def remove_pc(self, mac):
-        self.pcs.pop(mac)
+        self.pcs_pos.pop(mac)
         with open('locations.json', 'w') as f:
-            json.dump(self.pcs, f)
+            json.dump(self.pcs_pos, f)
 
-class PCIcon(tk.Canvas):
-    def __init__(self, master, mac, pos, draggable = False, online = True):
+class Basic_PCIcon(tk.Canvas):
+    def __init__(self, master, mac, pos, online = True):
         super().__init__(master, width=130, height=130)
         
         self.mac = mac
-        self.draggable = draggable
         self.online = online
         self.pos = pos
+
         self.place(x = self.pos[0], y = self.pos[1])
         
+        self.create_label()
+        self.change_icon()
+
+        
+    def create_label(self):
         self.label = tk.Label(self, text = self.mac)
         self.label.place(x = 0, y = 110)
+
+    def change_icon(self):
+        if self.online:
+            self.create_image(10,5, anchor = tk.NW, image = PC_ICON_ONLINE)
+        else:
+            self.create_image(10,5, anchor = tk.NW, image = PC_ICON_OFFLINE)
+
+class PCIcon_Edit_Mode(Basic_PCIcon):
+    def __init__(self, master, mac, pos, online = True):
+        super().__init__(master, mac, pos, online = online)
         
-        """self.delete_button = tk.Button(self, image = X_ICON, command = self.destroy, borderwidth=0, highlightthickness = 0, bd = 0, pady=0, padx=0)
-        self.delete_button.place(x = 110, y = 0)"""
+        self.create_delete_button()
         
+        self.make_draggable()
+    
+    def create_delete_button(self):
         self.delete_button = tk.Label(self, image = X_ICON)
         self.delete_button.place(x = 110, y = 0)
-        #self.delete_button.configure(bg = '')
         self.delete_button.bind("<Button-1>", lambda event: self.delete_pc())
-        
-        self.change_icon()
-        if self.draggable:
-            self.make_draggable()
-    
+
+   
+
     def make_draggable(self):
         self.bind("<Button-1>", self.on_click)
         self.bind("<B1-Motion>", self.on_drag)
@@ -81,16 +164,39 @@ class PCIcon(tk.Canvas):
     def on_release(self, event):
         self.master.change_location(self.mac, self.pos)
     
-    def change_icon(self):
-        if self.online:
-            self.create_image(10,5, anchor = tk.NW, image = PC_ICON_ONLINE)
-        else:
-            self.create_image(10,5, anchor = tk.NW, image = PC_ICON_OFFLINE)
-    
     def delete_pc(self):
-        self.master.remove_pc(self.mac)
-        super().destroy()
+        response = messagebox.askquestion("Delete PC", "Are you sure you want to delete this PC?", icon = 'warning')
+        if response == 'yes':
+            self.master.remove_pc(self.mac)
+            super().destroy()
+
+class PCIcon_View_Mode(Basic_PCIcon):
+    def __init__(self, master, mac, pos, online = True):
+        super().__init__(master, mac, pos, online = online)
+        self.bind("<Button-1>", self.on_click)
+
+    def on_click(self, event):
+        x = self.winfo_x()
+        y = self.winfo_y()
+        self.master.assign_dropdown(self.mac)
+         
+class DropDownMenu(tk.Listbox):
+    def __init__(self, master):
+        text_font = font.Font(family = "Calibri", size = 13)
+        super().__init__(master, width = 11, height = 3, selectmode = tk.SINGLE, font=text_font)
+        self.mac = None
+        self.insert(0,"See Screen")
+        self.insert(1,"Delete")
+        self.bind("<<ListboxSelect>>", self.on_select)
     
-main = tk.Tk()
-app = SetupWindow(master=main)
-app.mainloop()
+    def on_select(self, event):
+        selection = self.curselection()
+        print(self.get(selection[0]))
+    
+    def set_mac(self, mac):
+        self.mac = mac
+    
+    def get_mac(self):
+        return self.mac
+
+main = Main_Window()
--- basic server-client encrypted/ScreenShare.py
+++ basic server-client encrypted/ScreenShare.py
@@ -1,296 +1,102 @@
-from mss import mss
-import time
-from PIL import Image
-from zlib import compress, decompress
 import socket
-from Crypto.Cipher import DES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
-import time
 import cv2
-from pyautogui import screenshot
 import numpy as np
+import time
+import threading
 import pyautogui
-from basics import Cipher_ECB, Encrypted_TCP_Socket, Useful_Functions
-from threading import Thread, Lock
-from logger import Logger
+import pickle
+from basics import Cipher_ECB
+import dxcam
 
-logger = Logger()
-logger_name = 'ScreenShare'
-logger.create_logger(logger_name)
 
-MULTIPLE = 0.8
 RESULUTIONS = (1536, 864)
 QUALITY = 95
-
-class Encrypted_TCP_Server_For_ScreenShare:
-    def __init__(self, local_ip, local_port, key):
-        """This function initializes the ScreenShare
-        """
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((local_ip, local_port))
-
-        self.logger_name = 'Encrypted_TCP_Server_For_ScreenShare'
-        logger.create_logger(self.logger_name, 'ScreenShare')
-        self.cipher = Cipher_ECB(key)
-        self.connected = False
-        self.start()
-    
-    def start(self):
-        """This function handles the connection
-        """
-        self.socket.listen(1)
-        logger.log_debug('Listening for connections', self.logger_name)
-        self.client_socket, self.address = self.socket.accept()
-        print('Connected to: ' + str(self.address))
-        self.connected = True
-
-    def send_data(self, msg, packet_size=4096):
-        """This function sends data to the client
-
-        Args:
-            data (bytes): The data to send.
-        """
-        if type(msg) == str:
-            msg = msg.encode()
-
-        #ciphertext = self.cipher.encrypt(msg)
-        ciphertext = msg
-        packets = Useful_Functions.split_data(ciphertext, packet_size=packet_size)
-        first_packet = len(packets).to_bytes(1, byteorder='big')
-        first_packet += packet_size.to_bytes(2, byteorder='big')
-        first_packet += len(packets[-1]).to_bytes(2, byteorder='big')
-        #first_packet = self.cipher.encrypt(first_packet)
-
-        first_packet = first_packet
-
-        print(first_packet, len(first_packet))
-
-        self.client_socket.send(first_packet)
+PACKET_SIZE = 65504
+HEADER_SIZE = 7
+class Sender:
+    def __init__(self, local_ip, local_port, dest_ip, dest_port, key):
+        self.local_ip = local_ip
+        self.local_port = local_port
+        self.dest_ip = dest_ip
+        self.dest_port = dest_port
         
-        for packet in packets:
-            self.client_socket.send(packet)
-
-    def recv_data(self, packet_size=4096):
-        
-        full_data = b''
-        data = self.socket.recv(5)
-
-        data = self.cipher.decrypt(data)
-        num_packets = int(data[:4], 16)
-        packet_size = int(data[4:8], 16)
-        last_packet_size = int(data[8:12], 16)
-        for i in range(num_packets-1):
-            data = self.socket.recv(packet_size)
-            full_data += data
-
-        data = self.socket.recv(last_packet_size)
-        full_data += data
-
-        full_data = self.cipher.decrypt(full_data)
-        return full_data
- 
-class ScreenShare_Transmitter:
-    def __init__(self, local_ip, local_port, key, x_res=RESULUTIONS[0], y_res=RESULUTIONS[1]):
-        """This function initializes the ScreenShare
-        """
-        self.socket = Encrypted_TCP_Server_For_ScreenShare(local_ip, local_port, key)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((self.local_ip, self.local_port))
         self.stream = True
-        self.logger_name = 'ScreenShare_Viewer'
-        self.socket.logger_name = self.logger_name
-        logger.create_logger(self.logger_name, 'ScreenShare')
+
         self.frames = []
+        self.cam = dxcam.create()
+        self.cam.start(target_fps = 24)
+
+        self.key = key
+        self.cipher = Cipher_ECB(self.key)
 
     def start_stream(self):
-        """This function starts the ScreenShare
-        """
-        i = 0
-        while not self.socket.connected:
-            pass
-        
         while self.stream:
-            self.img = self.take_screenshot()
-            self.socket.send_data(self.img)
-            i += 1
-            logger.log_debug('Sent frame: ' + str(i), self.logger_name)
-            
+            frame = self.take_screenshot()
+            packets = self.split_into_packets(frame)
+            self.send_data(packets)
+        
     def take_screenshot(self):
         """This function takes a screenshot of the screen.
 
         Returns:
             bytes: The screenshot of the screen.
         """
-        frame = pyautogui.screenshot()
+        """frame = pyautogui.screenshot()
         frame = np.array(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, RESULUTIONS, interpolation=cv2.INTER_AREA)"""
+        frame = self.cam.get_latest_frame()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.resize(frame, RESULUTIONS, interpolation=cv2.INTER_AREA)
-        result, frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), QUALITY])
+        result, frame = cv2.imencode('.jpg', frame)
         frame = frame.tobytes()
         return frame
 
-class Encrypted_TCP_Client_For_ScreenShare:
-    def __init__(self, ip, port, key):
-        """This function initializes the ScreenShare
+    def split_into_packets(self, data):
+        """This function splits the data into packets.
+
+        Args:
+            data (bytes): The data to split.
+
+        Returns:
+            list: The list of packets.
         """
-        self.logger_name = 'Encrypted_TCP_Client_For_ScreenShare'
-        logger.create_logger(self.logger_name, 'ScreenShare')
+        data = data + b'0'*(PACKET_SIZE - len(data) % PACKET_SIZE)
+        data_size_in_packet = PACKET_SIZE - HEADER_SIZE
+        packets = [data[i:i+data_size_in_packet] for i in range(0, len(data)-data_size_in_packet, data_size_in_packet)]
+        num_of_packets = len(packets).to_bytes(2, 'big')
+        len_data = len(data).to_bytes(3, 'big')
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.ip = ip
-        self.port = port
-        self.connected = False
-        self.socket.connect((self.ip, self.port))
-        print('Connected to ' + self.ip + ':' + str(self.port))
-        self.connected = True
-        self.cipher = Cipher_ECB(key)
+        for i in range(len(packets)):
+            packets[i] = len_data + num_of_packets + i.to_bytes(2, 'big') + packets[i]
+            packets[i] = self.cipher.encrypt(packets[i])
+        #print(len(packets[0]), len(packets))
+        """first = str(hex(len(packets))).zfill(4).encode()
+        packets.insert(0, first)"""
         
-    def send_data(self, msg, packet_size=4096):
+        packets = packets[::-1]
+
+        return packets
+
+    def send_data(self, packets):
         """This function sends data to the client
 
         Args:
             data (bytes): The data to send.
         """
-        if type(msg) == str:
-            msg = msg.encode()
 
-        ciphertext = self.cipher.encrypt(msg)
-        packets = Useful_Functions.split_data(ciphertext, packet_size=packet_size)
-        first_packet = str(hex(len(packets))).encode().replace(b'0x', b'').zfill(4)
-        first_packet += str(hex(packet_size)).encode().replace(b'0x', b'').zfill(4)
-        first_packet += str(hex(len(packets[-1]))).encode().replace(b'0x', b'').zfill(4)
-        first_packet = self.cipher.encrypt(first_packet)
-
-        first_packet = first_packet + b'FIRST_PACKET'
-
-        self.socket.sendall(first_packet)
-        
         for packet in packets:
-            self.socket.sendall(packet)
-
-    def recv_data(self):
-        
-        full_data = b''
-        data = self.socket.recv(28)
-        data = data[:-12]
-        data = self.cipher.decrypt(data)
-        num_packets = int(data[:4], 16)
-        packet_size = int(data[4:8], 16)
-        last_packet_size = int(data[8:12], 16)
-
-        for i in range(num_packets-1):
-            data = self.socket.recv(packet_size)
-            full_data += data
-
-        data = self.socket.recv(last_packet_size)
-        full_data += data
-
-        full_data = self.cipher.decrypt(full_data)
-        return full_data
-
-class ScreenShare_Viewer:
-    def __init__(self, local_ip, local_port, key, fullscreen = False, x_res=RESULUTIONS[0], y_res=RESULUTIONS[1]):
-        """This function initializes the ScreenShare
-        """
-        self.socket = Encrypted_TCP_Client_For_ScreenShare(local_ip, local_port, key)
-        self.stream = True
-        self.logger_name = 'ScreenShare_Viewer'
-        self.socket.logger_name = self.logger_name
-        self.img = None
-        self.frame_num = 0
-        logger.create_logger(self.logger_name, 'ScreenShare')
-    
-    def recv_frame(self):
-        """This function starts the ScreenShare
-        """
-        i = 0
-        err_count = 0
-
-        try:
-            img = self.socket.recv_data()
-        except Exception as e:
-            img = None
-            err_count += 1
-            print(e)
-            print('Error count: ' + str(err_count))
-
-
-        if img is not None:
-            self.img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
-            self.frame_num += 1
-        
-        
-        logger.log_debug('Received frame: ' + str(i), self.logger_name)
-
-    def show_stream(self):
-        """This function starts the ScreenShare
-        """
-        state = 1
-        while not self.socket.connected:
-            pass
-        
-        while self.img is None:
-            pass
-        start = time.time()
-        while self.stream and state >= 0:
-            try:
-                state = cv2.getWindowProperty('frame', 0)
-            except cv2.error:
-                state -= 1
-
+            self.s.sendto(packet, (self.dest_ip, self.dest_port))
+            time.sleep(0.001)
             
-            cv2.imshow('frame', self.img)
-            cv2.waitKey(1)
-        
-        print('Stream ended')
-        print('Frame amount: ' + str(self.frame_num))
-        print('Time: ' + str(time.time() - start))
-        print('FPS: ' + str(self.frame_num / (time.time() - start)))
-        self.stream = False
 
-    def start_stream(self):
-        """This function starts the ScreenShare
-        """
-        state = 1
-        while not self.socket.connected:
-            pass
-        
-        Thread(target=self.show_stream).start()
-
-        while self.stream:
-            self.recv_frame()
-            
-        self.stream = False
 
 def main():
-    """start = time.time()
-    cipher = Cipher_DES()
-    total_bytes = 0
-    state = 1
-    while state >= 0:
-        frame = pyautogui.screenshot()
-        frame = np.array(frame)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (1536, 864), interpolation=cv2.INTER_AREA)
-        result, frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-        frame = frame.tobytes()
-        total_bytes += len(frame)
-        self.img = cv2.imdecode(np.frombuffer(frame, np.uint8), cv2.IMREAD_COLOR)
-        try:
-            state = cv2.getWindowProperty('frame', 0)
-        except cv2.error:
-            state -= 1
-        cv2.imshow('frame', self.img)
-        print(state)
-        cv2.waitKey(1)
-            
-            #save image to file
+    sender = Sender('192.168.68.115', 25566, '192.168.68.132', 25565, b'1234567812345678')
+    sender.start_stream()
 
-    print(time.time() - start)
-    print(total_bytes/1024/1024)"""
-    
-    viewer = ScreenShare_Transmitter('0.0.0.0', 25565, b'1234567890123456')
-    viewer.start_stream()
-    
+
 if __name__ == '__main__':
     main()
