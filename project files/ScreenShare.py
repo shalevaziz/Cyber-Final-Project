12141a974c735204bbc754709e52248c5be800fd
shalevaziz <azizshalev@gmail.com>
2023-05-23 11:29:00 +0300

    bg fixes

--- project files/ScreenShare.py
+++ project files/ScreenShare.py
@@ -12,16 +12,7 @@ HEADER_SIZE: int = 7
 
 
 class Sender:
-    """A class that shares this PC's screen to a remote destination.
-
-    Attributes:
-        local_ip (str): The local IP address to bind the socket to.
-        local_port (int): The local port to bind the socket to.
-        dest_ip (str): The IP address of the remote destination.
-        dest_port (int): The port of the remote destination.
-        key (bytes): The encryption key to use for encrypting the data.
-    """
-    def __init__(self, local_ip: str, local_port: int, dest_ip: str, dest_port: int, key: bytes):
+    def __init__(self, local_ip, local_port, dest_ip, dest_port, key):
         self.local_ip = local_ip
         self.local_port = local_port
         self.dest_ip = dest_ip
@@ -31,42 +22,42 @@ class Sender:
         self.s.bind((self.local_ip, self.local_port))
         self.stream = True
 
+        self.frames = []
+        """self.cam = dxcam.create()
+        self.cam.start(target_fps = 24)"""
+
         self.key = key
         self.cipher = Cipher_ECB(self.key)
 
-    def listen_for_stop(self):
-        """A function that listens for a stop signal from the remote destination.
+    def wait_for_stop(self):
+        msg, _ = self.s.recvfrom(16)
+        if self.cipher.decrypt(msg) == b'STOP000000000000':
+            self.stream = False
 
-        If a stop signal is received, the stream is stopped.
-        """
-        while True:
-            data, addr = self.s.recvfrom(16)
-            data = self.cipher.decrypt(data)
-            if data == b'STOP000000000000':
-                self.stream = False
-                break
-    
     def start_stream(self):
-        """A function that starts the stream of screenshots.
-
-        This function takes screenshots of the screen, splits them into packets, and sends them to the remote destination.
-        """
-        threading.Thread(target=self.listen_for_stop).start()
+        threading.Thread(target=self.wait_for_stop).start()
         while self.stream:
             frame = self.take_screenshot()
             packets = self.split_into_packets(frame)
             self.send_data(packets)
-
+        print('stopped')
+        
     def take_screenshot(self):
-        """A function that takes a screenshot of the screen.
+        """This function takes a screenshot of the screen.
 
         Returns:
             bytes: The screenshot of the screen.
         """
+        """frame = pyautogui.screenshot()
+        frame = np.array(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, RESULUTIONS, interpolation=cv2.INTER_AREA)"""
         frame = np.array(pyautogui.screenshot())
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, RESOLUTIONS, interpolation=cv2.INTER_AREA)
-        return frame.tobytes()
+        frame = cv2.resize(frame, RESULUTIONS, interpolation=cv2.INTER_AREA)
+        result, frame = cv2.imencode('.jpg', frame)
+        frame = frame.tobytes()
+        return frame
 
     def split_into_packets(self, data):
         """This function splits the data into packets.
@@ -94,16 +85,16 @@ class Sender:
 
         return packets
 
-    def send_data(self, packets: list[bytes]):
-        """A function that sends the packets to the remote destination.
+    def send_data(self, packets):
+        """This function sends data to the client
 
         Args:
-            packets (list): A list of packets to send.
+            data (bytes): The data to send.
         """
-        for packet in packets:
-            self.s.sendto(self.cipher.encrypt(packet), (self.dest_ip, self.dest_port))
-            time.sleep(0.001)
 
+        for packet in packets:
+            self.s.sendto(packet, (self.dest_ip, self.dest_port))
+            time.sleep(0.001)
 class MultiSender(Sender):
     """A class that broadcasts this PC's screen to multiple remote destinations.
 
@@ -131,9 +122,8 @@ class MultiSender(Sender):
         """
         self.stream = False
         self.s.sendto(self.cipher.encrypt(b'STOP000000000000'), (self.dest_ip, self.dest_port))
-
 class Receiver:
-    def __init__(self, local_ip: str, local_port: int, key: bytes, student_mode: bool = False):
+    def __init__(self, local_ip, local_port, key):
         self.local_ip = local_ip
         self.local_port = local_port
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -141,8 +131,6 @@ class Receiver:
         
         self.cipher = Cipher_ECB(key)
         self.lock = threading.Lock()
-        
-        self.student_mode = student_mode
         
     def start_stream(self):
         """This function starts the stream.
@@ -162,14 +150,9 @@ class Receiver:
         packets = []
 
         self.lock.acquire()
-        first, addr = self.s.recvfrom(PACKET_SIZE)
-        self.dest_addr = addr
+        #s.sendto(b'1', ('192.168.68.113', 25566))
+        first, _ = self.s.recvfrom(PACKET_SIZE)
         first = self.cipher.decrypt(first)
-        if self.student_mode and first == b'STOP000000000000':
-            self.stream = False
-            self.lock.release()
-            return
-        
         data_len = int.from_bytes(first[:3], 'big')
         num_packets = int.from_bytes(first[3:5], 'big')
         packets.append(first[5:])
@@ -177,13 +160,8 @@ class Receiver:
         for i in range(num_packets-1):
             packet, _ = self.s.recvfrom(PACKET_SIZE)
             packet = self.cipher.decrypt(packet)
-            if self.student_mode and packet == b'STOP000000000000':
-                self.stream = False
-                self.lock.release()
-                return
-            
             packets.append(packet[5:])
-
+            #print(i)
         self.lock.release()
 
         packets = sorted(packets, key=lambda x: int.from_bytes(x[:2], 'big'))
@@ -196,10 +174,6 @@ class Receiver:
     def show_screenshots(self):
         """This function shows the screenshot.
         """
-        
-        if self.student_mode:
-            cv2.namedWindow('ScreenShare', cv2.WINDOW_NORMAL)
-            cv2.setWindowProperty('ScreenShare', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
                 
         state = 1
         
@@ -209,20 +183,18 @@ class Receiver:
         while self.stream:
             self.lock.acquire()
             img = cv2.imdecode(np.frombuffer(self.data, np.uint8), cv2.IMREAD_COLOR)
-            if not self.student_mode:
-                try:
-                    state = cv2.getWindowProperty('ScreenShare', 0)
-                except cv2.error as e:
-                    state -= 1
-                    print(state)
-                    print(e)
-
             try:
-                cv2.imshow('ScreenShare', img)
+                cv2.imshow('img', img)
             except:
                 pass
             self.lock.release()
             
+            try:
+                state = cv2.getWindowProperty('frame', 0)
+            except cv2.error as e:
+                state -= 1
+                print(state)
+                print(e)
             
             if state < 0:
                 self.stream = False
@@ -230,10 +202,6 @@ class Receiver:
                 break
             
             cv2.waitKey(1)
-        
-        if not self.student_mode:
-            msg = b'STOP000000000000'
-            self.s.sendto(self.cipher.encrypt(msg), self.dest_addr)
 
     def recv_frames(self):
         """This function continuously receives frames from the server.
